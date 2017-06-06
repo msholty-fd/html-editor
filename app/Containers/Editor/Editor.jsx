b72388dd4b82c12c459d7e0bfcefced9acdebc8c
Michael Sholty <michael.sholty@sharpspring.com>
2017-06-05 21:18:40 -0400

    feat: add froala-editor

--- app/Containers/Editor/Editor.jsx
+++ app/Containers/Editor/Editor.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import FroalaEditor from 'react-froala-wysiwyg';
 
-export default () => (
-  <h1>Editor</h1>
-);
+require('froala-editor/js/froala_editor.pkgd.min.js');
+require('froala-editor/css/froala_editor.pkgd.min.css');
+
+// Require Editor JS files.
+// require("froala-editor/js/froala_editor.pkgd.min.js");
+// require("froala-editor/css/froala_editor.pkgd.min.css");
+
+// Require Font Awesome.
+// require('font-awesome/css/font-awesome.css');
+
+// Include special components if required.
+// var FroalaEditorView = require('react-froala-wysiwyg/FroalaEditorView');
+// var FroalaEditorA = require('react-froala-wysiwyg/FroalaEditorA');
+// var FroalaEditorButton= require('react-froala-wysiwyg/FroalaEditorButton');
+// var FroalaEditorImg = require('react-froala-wysiwyg/FroalaEditorImg');
+// var FroalaEditorInput = require('react-froala-wysiwyg/FroalaEditorInput');
+
+export default () => <FroalaEditor tag="textarea" />;
